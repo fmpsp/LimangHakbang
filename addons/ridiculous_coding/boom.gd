e719668704c8cab5996fe40ce8d85997257d8aad
fmpsp <flordelizamatthewjason@gmail.com>
2025-01-28 22:06:29 +0800

    Initial Commit

---
+++ addons/ridiculous_coding/boom.gd
@@ -0,0 +1,18 @@
+@tool
+extends Node2D
+
+var destroy = false
+var last_key = ""
+
+func _ready():
+	$AudioStreamPlayer.play()
+	$AnimatedSprite2D.frame = 0
+	$AnimatedSprite2D.play("default")
+	$AnimationPlayer.play("default")
+	$Timer.start()
+	$Label.text = last_key
+	$Label.modulate = Color(randf_range(0,2), randf_range(0,2), randf_range(0,2))
+
+func _on_Timer_timeout():
+	if destroy:
+		queue_free()
