eac3083f68a3c215de13bbd503515e0f69f2021f
fmpsp <flordelizamatthewjason@gmail.com>
2025-03-17 03:33:38 +0800

    make footsteps color ramp transparent-color-transparent main-menu

--- code/internal/main_menu.gd
+++ code/internal/main_menu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+@onready var footsteps = $"Panel/Footstep Particles"
+
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	footsteps.emitting = true
+	pass
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Test-Room.tscn")
 
